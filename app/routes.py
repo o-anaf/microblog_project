c4291ed4def4da626b6cc1a9ed436dc2f8be9705
o-anaf <adlenenafi96@gmail.com>
2024-10-14 16:49:58 +0200

    Part 3 Webforms

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,9 @@
-
 from app import flask_app
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
+from app.forms import LoginForm
 
 @flask_app.route('/')
 @flask_app.route('/index')
-
-
-
 def index():
     user = {'username':'Adlène'}
     posts = [
@@ -14,3 +11,13 @@ def index():
         {'author':{'username':'user2'}, 'body':'working fine here'}
     ]
     return render_template('index.html', title='Home', user = user, posts=posts)
+
+
+@flask_app.route('/login', methods=['GET', 'POST'])
+def login():
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        flash('Login resquested for user {}, remember_me={}'.format(
+            login_form.username.data, login_form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', form=login_form)
