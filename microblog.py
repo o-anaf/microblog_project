022482dfe111f1002f92a30f01a47be70a1f164b
o-anaf <adlenenafi96@gmail.com>
2024-10-15 14:18:20 +0200

    Part 4 Database with SQLALCHEMY

--- microblog.py
+++ microblog.py
@@ -1,4 +1,13 @@
 from app import flask_app
 
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from app import flask_app, db
+from app.models import User, Post
+
+@flask_app.shell_context_processor
+def make_shell_context():
+    return('sa':sa, 'so':so, 'db':db, 'User':User, 'Post':Post)
+
 if __name__=='__main__':
     flask_app.run()
